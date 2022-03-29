4da9ab91ba134017d40ba279e191012bb902f999
Brenton Walker <brentonw94@yahoo.com.au>
2022-03-29 19:33:23 +1100

    [Brenton] Initial apollo server setup

---
+++ src/data-sources.ts
@@ -0,0 +1,14 @@
+import { RESTDataSource } from 'apollo-datasource-rest'
+
+
+export class OrderList extends RESTDataSource {
+    constructor() {
+        super();
+    }
+
+    // GET
+    async getList() {
+        // TODO excel sheet mapping
+    }
+
+}
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { ApolloServer } from "apollo-server";
+import { OrderList } from "./data-sources";
+import resolvers from './resolvers';
+import typeDefs from './type-defs';
+
+export const server = new ApolloServer({
+    resolvers,
+    typeDefs,
+    dataSources: () => {
+        return {
+          orderList: new OrderList()
+        };
+      },
+});
+
+server.listen({port: 3000}).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
